c366f6515d4f064cdded1a5c06c061284ce8e3b8
Frederik Schlemmer <Frederik.Schlemmer@adesso.de>
2018-05-31 13:27:08 +0200

    Git Ignore
    Trying again..

--- src/main/java/config/Config.java
+++
@@ -1,7 +0,0 @@
-package config;
-
-public class Config {
-
-    public static final String API_TOKEN = "f0328e1de07155bb662f1dace9efa813cab15c5c ";
-    public static final String API_URL = "https://api.github.com/graphql";
-}
