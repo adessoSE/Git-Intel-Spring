6f5a74d6289043e31bfaa30d265b04f4b6bea8d8
Dario Braun <dario.braun@adesso.de>
2018-07-27 09:28:16 +0200

    Add Cross Origin functionality for REST controller

--- src/main/java/REST/OrganizationController.java
+++ src/main/java/REST/OrganizationController.java
@@ -4,6 +4,7 @@ package REST;
 import objects.*;
 import objects.Team.Team;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@CrossOrigin
 public class OrganizationController {
 
     public enum Type {COMMITS, ISSUES, PRS}
