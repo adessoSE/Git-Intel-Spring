9d08cd301444ecf1c2c2c4f29a32cf8786840a89
Frederik Schlemmer <Frederik.Schlemmer@adesso.de>
2018-10-24 09:03:04 +0200

    Adds HTTPStatus to ResponseEntity

--- src/main/java/de/adesso/gitstalker/core/REST/OrganizationController.java
+++ src/main/java/de/adesso/gitstalker/core/REST/OrganizationController.java
@@ -6,6 +6,7 @@ import de.adesso.gitstalker.core.objects.*;
 import de.adesso.gitstalker.core.repositories.OrganizationRepository;
 import de.adesso.gitstalker.core.repositories.RequestRepository;
 import de.adesso.gitstalker.core.requests.RequestManager;
+import de.adesso.gitstalker.core.resources.organization_validation.ResponseOrganizationValidation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -32,81 +33,88 @@ public class OrganizationController {
 
     /**
      * Check if organization is already stored in data base. If not, initiate GitHub crawl for requested organization.
-     * <p>
      *
      * @return
      */
-//    @RequestMapping("/organizationdetail/{organizationName}")
-//    public OrganizationDetail retrieveOrganizationDetail(@PathVariable String organizationName) {
-//        String formattedName = this.formatInput(organizationName);
-//        if (this.checkIfDataAvailable(formattedName)) {
-//            return this.organizationRepository.findByOrganizationName(formattedName).getOrganizationDetail();
-//        } else return null;
-//    }
-//
-//    @RequestMapping("/organizationobject/{organizationName}")
-//    public OrganizationWrapper retrieveOrganizationObject(@PathVariable String organizationName) {
-//        String formattedName = this.formatInput(organizationName);
-//        if (this.checkIfDataAvailable(formattedName)) {
-//            return this.organizationRepository.findByOrganizationName(formattedName);
-//        } else return null;
-//    }
-//
-//    @RequestMapping("/members/{organizationName}")
-//    public Collection<Member> retrieveOrganizationMembers(@PathVariable String organizationName) {
-//        String formattedName = this.formatInput(organizationName);
-//        if (this.checkIfDataAvailable(formattedName)) {
-//            return this.organizationRepository.findByOrganizationName(formattedName).getMembers().values();
-//        } else return null;
-//    }
-//
-//    @RequestMapping("/repositories/{organizationName}")
-//    public Collection<Repository> retrieveOrganizationRepositories(@PathVariable String organizationName) {
-//        String formattedName = this.formatInput(organizationName);
-//        if (this.checkIfDataAvailable(formattedName)) {
-//            return this.organizationRepository.findByOrganizationName(formattedName).getRepositories().values();
-//        } else return null;
-//    }
-//
-//    @RequestMapping("/externalrepositories/{organizationName}")
-//    public Collection<Repository> retrieveExternalRepositories(@PathVariable String organizationName) {
-//        String formattedName = this.formatInput(organizationName);
-//        if (this.checkIfDataAvailable(formattedName)) {
-//            return this.organizationRepository.findByOrganizationName(formattedName).getExternalRepos().values();
-//        } else return null;
-//    }
+    @RequestMapping("/organizationdetail/{organizationName}")
+    public ResponseEntity<OrganizationDetail> retrieveOrganizationDetail(@PathVariable String organizationName) {
+        String formattedName = this.formatInput(organizationName);
+        OrganizationWrapper organization = this.organizationRepository.findByOrganizationName(formattedName);
+
+        return (ResponseEntity<OrganizationDetail>) this.processResponseEntity(RequestType.ORGANIZATION_DETAIL, organization, this.checkStatusOfRequestedInformation(formattedName));
+    }
+
+    @RequestMapping("/members/{organizationName}")
+    public ResponseEntity<Collection<Member>> retrieveOrganizationMembers(@PathVariable String organizationName) {
+        String formattedName = this.formatInput(organizationName);
+        OrganizationWrapper organization = this.organizationRepository.findByOrganizationName(formattedName);
+
+        return (ResponseEntity<Collection<Member>>) this.processResponseEntity(RequestType.MEMBER, organization, this.checkStatusOfRequestedInformation(formattedName));
+    }
+
+    @RequestMapping("/repositories/{organizationName}")
+    public ResponseEntity<Collection<Repository>> retrieveOrganizationRepositories(@PathVariable String organizationName) {
+        String formattedName = this.formatInput(organizationName);
+        OrganizationWrapper organization = this.organizationRepository.findByOrganizationName(formattedName);
+
+        return (ResponseEntity<Collection<Repository>>) this.processResponseEntity(RequestType.REPOSITORY, organization, this.checkStatusOfRequestedInformation(formattedName));
+    }
+
+    @RequestMapping("/externalrepositories/{organizationName}")
+    public ResponseEntity<Collection<Repository>> retrieveExternalRepositories(@PathVariable String organizationName) {
+        String formattedName = this.formatInput(organizationName);
+        OrganizationWrapper organization = this.organizationRepository.findByOrganizationName(formattedName);
+
+        return (ResponseEntity<Collection<Repository>>) this.processResponseEntity(RequestType.EXTERNAL_REPO, organization, this.checkStatusOfRequestedInformation(formattedName));
+    }
 
     @RequestMapping("/teams/{organizationName}")
     public ResponseEntity<Collection<Team>> retrieveOrganizationTeams(@PathVariable String organizationName) {
         String formattedName = this.formatInput(organizationName);
         OrganizationWrapper organization = this.organizationRepository.findByOrganizationName(formattedName);
 
-        return (ResponseEntity<Collection<Team>>) this.processResponseEntity("Team", organization, this.checkStatusOfRequestedInformation(formattedName));
+        return (ResponseEntity<Collection<Team>>) this.processResponseEntity(RequestType.TEAM, organization, this.checkStatusOfRequestedInformation(formattedName));
     }
 
-//    @RequestMapping("/createdreposbymembers/{organizationName}")
-//    public Collection<ArrayList<Repository>> retrieveCreatedReposByOrganizationMembers(@PathVariable String organizationName) {
-//        String formattedName = this.formatInput(organizationName);
-//        if (this.checkIfDataAvailable(formattedName)) {
-//            return this.organizationRepository.findByOrganizationName(formattedName).getCreatedReposByMembers().values();
-//        } else return null;
-//    }
+    @RequestMapping("/createdreposbymembers/{organizationName}")
+    public ResponseEntity<Collection<ArrayList<Repository>>> retrieveCreatedReposByOrganizationMembers(@PathVariable String organizationName) {
+        String formattedName = this.formatInput(organizationName);
+        OrganizationWrapper organization = this.organizationRepository.findByOrganizationName(formattedName);
+
+        return (ResponseEntity<Collection<ArrayList<Repository>>>) this.processResponseEntity(RequestType.CREATED_REPOS_BY_MEMBERS, organization, this.checkStatusOfRequestedInformation(formattedName));
+    }
 
     @RequestMapping("/**")
     public ResponseEntity<Object> response404Error() {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    private ResponseEntity<?> processResponseEntity(String requestType, OrganizationWrapper organization, HttpStatus httpStatus){
+    private ResponseEntity<?> processResponseEntity(RequestType requestType, OrganizationWrapper organization, HttpStatus httpStatus){
         if (httpStatus.is2xxSuccessful()) {
             switch (requestType){
-                case "Team":
+                case TEAM:
                     Collection<Team> teams = organization.getTeams().values();
                     return new ResponseEntity<>(teams, httpStatus);
+                case CREATED_REPOS_BY_MEMBERS:
+                    Collection<ArrayList<Repository>> createdReposByMembers = organization.getCreatedReposByMembers().values();
+                    return new ResponseEntity<>(createdReposByMembers, httpStatus);
+                case EXTERNAL_REPO:
+                    Collection<Repository> externalRepositories = organization.getExternalRepos().values();
+                    return new ResponseEntity<>(externalRepositories, httpStatus);
+                case REPOSITORY:
+                    Collection<Repository> organizationRepositories = organization.getRepositories().values();
+                    return new ResponseEntity<>(organizationRepositories, httpStatus);
+                case MEMBER:
+                    Collection<Member> organizationMember = organization.getMembers().values();
+                    return new ResponseEntity<>(organizationMember, httpStatus);
+                case ORGANIZATION_DETAIL:
+                    OrganizationDetail organizationDetail = organization.getOrganizationDetail();
+                    return new ResponseEntity<>(organizationDetail, httpStatus);
             }
         }
         return new ResponseEntity<>(httpStatus);
     }
+
     /**
      * Method used to check if there is already requested information available. If there are no requests running for the requested organization then the de.adesso.gitstalker.core.de.adesso.gitstalker.requests are generated.
      *
@@ -129,7 +137,7 @@ public class OrganizationController {
         Query validationQuery = new RequestManager(organizationName).generateRequest(RequestType.ORGANIZATION_VALIDATION);
         validationQuery.crawlQueryResponse();
         this.requestRepository.save(validationQuery);
-        if(validationQuery.getQueryResponse().getResponseOrganizationValidation().getData().getOrganization() != null){
+        if(((ResponseOrganizationValidation) validationQuery.getQueryResponse()).getData().getOrganization() != null){
             return HttpStatus.PROCESSING;
         } else return HttpStatus.BAD_REQUEST;
     }
