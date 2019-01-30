23c2270738997d6be7ba54613fce52ecb2ea6b75
Frederik Schlemmer <Frederik.Schlemmer@adesso.de>
2019-01-30 15:52:54 +0100

    Delete unused constructor

--- src/main/java/de/adesso/gitstalker/core/objects/OrganizationDetail.java
+++ src/main/java/de/adesso/gitstalker/core/objects/OrganizationDetail.java
@@ -35,18 +35,6 @@ public class OrganizationDetail {
     private LinkedHashMap<String, Integer> memberAmountHistory;
     private Date lastUpdate;
 
-    public OrganizationDetail(String name, String description, String websiteURL, String githubURL, String location, String avatarURL, int numOfMembers, int numOfRepositories, int numOfTeams) {
-        this.setName(name);
-        this.setDescription(description);
-        this.setWebsiteURL(websiteURL);
-        this.setGithubURL(githubURL);
-        this.setLocation(location);
-        this.setAvatarURL(avatarURL);
-        this.setNumOfMembers(numOfMembers);
-        this.setNumOfRepositories(numOfRepositories);
-        this.setNumOfTeams(numOfTeams);
-    }
-
     public OrganizationDetail addMemberAmountHistory(int currentMemberAmount) {
         if (Objects.isNull(this.memberAmountHistory)){
             this.memberAmountHistory = new LinkedHashMap<>();
