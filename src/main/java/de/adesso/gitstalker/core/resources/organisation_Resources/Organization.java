cf19595e9ed309d9f488435b7ba73b08462d402f
Frederik Schlemmer <Frederik.Schlemmer@adesso.de>
2019-01-31 10:27:37 +0100

    Fix wrong JsonProperty in organization

--- src/main/java/de/adesso/gitstalker/core/resources/organisation_Resources/Organization.java
+++ src/main/java/de/adesso/gitstalker/core/resources/organisation_Resources/Organization.java
@@ -1,7 +1,9 @@
 package de.adesso.gitstalker.core.resources.organisation_Resources;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 @Data
 @NoArgsConstructor
@@ -10,6 +12,7 @@ public class Organization {
     private String name;
     private String location;
     private String websiteUrl;
+    @JsonProperty("url")
     private String githubUrl;
     private String avatarUrl;
     private String description;
